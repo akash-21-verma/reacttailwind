e53642cd13102b6925f688f80f48f63e5429ba68
Akash Verma <72678959+akash-21-verma@users.noreply.github.com>
2021-06-22 10:47:00 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import {Link} from "react-router-dom"
 const Navbar = ({toggle}) => {
     return ( 
         <nav className="flex justify-between items-center h-16 bg-white text-black relative shadow-sm font-mono" role="navigation">
-            <Link to="/" className="pl-10"> EGG </Link>
+            <Link to="/reacttailwind" className="pl-10"> EGG </Link>
             <div className="px-4 cursor-pointer md:hidden" onClick={toggle}>
                 &xi;
             </div>
             <div className="pr-8 md:block hidden">
-                <Link className="p-4" to="/"> Home </Link>
+                <Link className="p-4" to="/reacttailwind"> Home </Link>
                 <Link className="p-4" to="/about"> About </Link>
                 <Link className="p-4" to="/contact"> Contact </Link>
             </div>
